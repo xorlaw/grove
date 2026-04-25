28abf9b9af3347122d1bb43b67b74e634b167651
xor <xor@morke.org>
2026-04-25 11:02:20 +0100

    add main structs in grovefile parser

--- src/internal/grovefile/grovefile.go
+++ src/internal/grovefile/grovefile.go
@@ -9,4 +9,36 @@ import (
 	"github.com/BurntSushi/toml"
 )
 
+const Filename = "Grovefile"
+
+type Grovefile struct {
+	Package 	PackageMeta `toml:"package"`
+	Source		Source		`toml:"source"`
+	Build		Build		`toml:"build"`
+	Deps		Deps		`toml:"deps"`
+}
+
+type PackageMeta struct {
+	Name		string `toml:"name"`
+	Version 	string `toml:"version"`
+	Desc		string `toml:"desc"`
+	Author		string `toml:"author"`
+	License		string `toml:"license"`
+}
+
+type Source struct {
+	Primary		string `toml:"primary"`
+	Fallback 	string `toml:"fallback"`
+}
+
+type Build struct {
+	Cmd			string `toml:"cmd"`
+	Install 	string `toml:"install"`
+	Prefix		string `toml:"prefix"`
+}
+
+type Deps struct {
+	Requires	[]string `toml:"requires"`
+}
+
 
