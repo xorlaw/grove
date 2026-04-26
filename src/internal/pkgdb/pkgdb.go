add8ae0e297d8df77a291e180ec02c28495192d4
xor <xor@morke.org>
2026-04-26 19:40:36 +0100

    add logic for saving stuff to pkgdb

--- src/internal/pkgdb/pkgdb.go
+++ src/internal/pkgdb/pkgdb.go
@@ -42,4 +42,57 @@ func Load(path string) (*DB, error) {
 	return &db, nil
 }
 
+func Save(path string, db *DB) error {
+	if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+		return fmt.Errorf("creating db directory: %w", err)
+	}
+
+	tmp := path + ".tmp"
+
+	f, err := os.OpenFIle(tmp. os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return fmt.Errorf("opening temp db file: %w", err)
+	}
+
+	if err := toml.NewEncoder(f).Encode(db); err != nil {
+		f.Close()
+		os.Remove(tmp)
+		return fmt.Errorf("encoding packages.toml: %w", err)
+	}
+
+	f.Close()
+	
+	if err := os.Rename(tmp, path); err != nil {
+		os.Remove(tmp)
+		return fmt.Errorf("saving packages.toml: %w", err)
+	}
+
+	return nil
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
